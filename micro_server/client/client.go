837041a7516241a3693e127cbe2a657a6f249f3f
Baha_Zhangbei <zhangbei@bahamutgame.com>
2022-10-18 20:22:15 +0800

    client

--- micro_server/client/client.go
+++ micro_server/client/client.go
@@ -1,1 +1,17 @@
 package client
+
+import (
+	"micro_server/rpc"
+	"micro_server/server"
+	"sync"
+)
+
+type Client struct {
+	cc      rpc.Proxy
+	opt     *server.Option
+	sending sync.Mutex
+	header  rpc.Header
+	mu      sync.Mutex
+	seq     uint64
+	pending map[uint64]*Call
+}
